8835dc5640946172b4d24acf8a1486ba3893a9f4
Lyubov Trushnikova <lovelly2008@ya.ru>
2022-12-07 00:16:55 +0500

    fix: правки после второго ревью

--- scripts/index.js
+++ scripts/index.js
@@ -3,9 +3,8 @@ let openPopupProfile = document.querySelector('.profile__button-edit');
 let closePopupProfile = document.querySelector('.popup__btn-close');
 let popupProfile = document.querySelector('.popup');
 let formaPopup = popupProfile.querySelector('.popup__forma');
-let namePopupInput = formaPopup.querySelector('.popup__input_name');
-let profPopupInput = formaPopup.querySelector('.popup__input_prof');
-let savePopupButton = popupProfile.querySelector('.popup__btn-save-profile')
+let namePopupInput = formaPopup.querySelector('.popup__name');
+let profPopupInput = formaPopup.querySelector('.popup__prof');
 let nameProfile = document.querySelector('.profile__info-title');
 let profProfile = document.querySelector('.profile__info-subtitle');
 
