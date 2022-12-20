5df05d0b7e9e4e713ebb7a1bb42955870a012b1a
Lyubov Trushnikova <lovelly2008@ya.ru>
2022-12-21 02:54:31 +0500

    5 проект сделана кнопка delete, лайк и картинка

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,15 @@
 !function(e){"function"!=typeof e.matches&&(e.matches=e.msMatchesSelector||e.mozMatchesSelector||e.webkitMatchesSelector||function(e){for(var t=this,o=(t.document||t.ownerDocument).querySelectorAll(e),n=0;o[n]&&o[n]!==t;)++n;return Boolean(o[n])}),"function"!=typeof e.closest&&(e.closest=function(e){for(var t=this;t&&1===t.nodeType;){if(t.matches(e))return t;t=t.parentNode}return null})}(window.Element.prototype);
-let openPopupProfile = document.querySelector('.profile__button-edit');
-let closePopup = document.querySelectorAll('.popup__btn-close');
-let popupProfile = document.querySelector('.popup__profile');
-let formaPopup = popupProfile.querySelector('.popup__forma');
-let namePopupInput = formaPopup.querySelector('.popup__input_type_name');
-let profPopupInput = formaPopup.querySelector('.popup__input_type_prof');
-let nameProfile = document.querySelector('.profile__info-title');
-let profProfile = document.querySelector('.profile__info-subtitle');
+const openPopupProfile = document.querySelector('.profile__button-edit');
+const closePopup = document.querySelectorAll('.popup__btn-close');
+const popupProfile = document.querySelector('.popup__profile');
+const formaPopup = popupProfile.querySelector('.popup__forma');
+const namePopupInput = formaPopup.querySelector('.popup__input_type_name');
+const profPopupInput = formaPopup.querySelector('.popup__input_type_prof');
+const nameProfile = document.querySelector('.profile__info-title');
+const profProfile = document.querySelector('.profile__info-subtitle');
+const popupImgView = document.querySelector('.popup_img_view');
 
-
+console.log(popupImgView)
 
 openPopupProfile.addEventListener('click', function() {
     popupProfile.classList.add('popup_opened');
@@ -71,32 +72,49 @@ const initialCards = [
   const elements = document.querySelector('.elements');
   const templateElement = document.querySelector('#template-element');
 
-  //готовим форму для карточек
+  //создаем форму для элементов и удаление элемента
   const creatCard = (link, name) => {
    const elementCard = templateElement.content.querySelector('.element').cloneNode(true);
    elementCard.querySelector('.element__img').src = link;
    elementCard.querySelector('.element__info-text').textContent = name;
-   return elementCard;
+
+   //работа кнопки delete
+   elementCard.querySelector('.element__delete').addEventListener('click', () => {
+   elementCard.remove();
+  });
+
+  //работа лайка
+  elementCard.querySelector('.element__info-heart').addEventListener('click', (event) => {
+  event.target.classList.toggle('element__info-heart_active');
+  });
+
+  //oткрытие image
+  const elementImg = elementCard.querySelector('.element__img')
+  elementImg.addEventListener('click', function() {
+    popupImgView.classList.add('popup_opened');
+    popupImgView.querySelector('.popup__image').src = link;
+    popupImgView.querySelector('.popup__caption').textContent = name;
+    
+  });
+  return elementCard;
   }
 
   //функция которая добавляет в контейнер elements элементы html
   const renderCard = (link, name) => {
-    console.log(name + ' ' + link)
     elements.prepend(creatCard(link, name))
   }
-//перебор элементов массива методом forEach
+  //перебор элементов массива методом forEach
   initialCards.forEach((val, i) => {
     renderCard(val.link, val.name);
   })
 
   //найдем input для ввода данных
- const formaAdd = document.querySelector('.popup__forma-add');
- const inputAddName = formaAdd.querySelector('.popup__input_add_name');
- const inputAddLink = formaAdd.querySelector('.popup__input_add_link');
+  const formaAdd = document.querySelector('.popup__forma-add');
+  const inputAddName = formaAdd.querySelector('.popup__input_add_name');
+  const inputAddLink = formaAdd.querySelector('.popup__input_add_link');
 
- 
- //функция сохранения данных
- const saveAdd = (event) => {
+  //функция сохранения данных
+  const saveAdd = (event) => {
   event.preventDefault();
   //nameAdd.textContent = inputAddName.value;
   //imgAdd.src = inputAddLink.value;
@@ -107,9 +125,7 @@ const initialCards = [
  }
  formaAdd.addEventListener( 'submit', saveAdd);
 
-
-
-  //2. форма открытия popup
+  // форма открытия popup
   const openPopupAdd = document.querySelector('.profile__button-add');
   const popupAdd = document.querySelector('.popup__add');
   const formaPopupAdd = popupAdd.querySelector('.popup__forma');
